4a07b6323b50ac2ab9cb19d19df24c9026adbb6f
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-03 19:51:32 +0300

    jwt cloudinary added .env and backtop button added

--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -4,6 +4,7 @@ import MainFooter from '../components/footer/MainFooter'
 import { useDispatch } from 'react-redux'
 
 import { GetCategoriesName } from '../redux/actions/categoryActions'
+import BackTopButton from '../components/backtop/BackTopButton'
 
 const MainLayout = ({children}) => {
     
@@ -19,6 +20,7 @@ const MainLayout = ({children}) => {
         <div className='container mx-auto'>
         {children}
         </div>
+        <BackTopButton />
         <MainFooter />
     </Fragment>
   )
