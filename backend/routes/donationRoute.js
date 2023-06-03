1dce57b07e53219fdc9ca5848c3e19339eeca3f6
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-03 19:48:41 +0300

    added create donation route

--- backend/routes/donationRoute.js
+++ backend/routes/donationRoute.js
@@ -1,8 +1,9 @@
 var express = require('express');
+const { createDonation } = require('../controllers/donationController');
 
 
 var router = express.Router();
 
-
+router.route('/create-donation').post(createDonation)
 
 module.exports = router;
