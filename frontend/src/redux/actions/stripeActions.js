847dba8ed20b3b42d197b059b3e12b62d37b866f
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-04 11:34:30 +0300

    created stripe actions

---
+++ frontend/src/redux/actions/stripeActions.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { STRIPE_APPLY_FAIL, STRIPE_APPLY_REQUEST, STRIPE_APPLY_SUCCESS } from "../constants/stripeConstants";
+
+export const CreatePaymentIntent =  (stripeItem) => async(dispatch) => {
+
+    try {
+
+        dispatch({type: STRIPE_APPLY_REQUEST})
+
+   
+
+        
+        const {data }=  await axios.post('http://localhost:5000/api/stripe-donate',stripeItem)
+  
+        //   dispatch success
+          dispatch({
+              type: STRIPE_APPLY_SUCCESS,
+              payload : data
+          })
+
+    } catch (error) {
+        dispatch({
+            type: STRIPE_APPLY_FAIL,
+            payload : error.response.data.error
+        })
+    }
+
+    
+
+}
