d23a6e2bc6ad322470820c38a2b8f260899b577d
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-04 11:34:06 +0300

    create donation action created

---
+++ frontend/src/redux/actions/donationActions.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { CREATE_DONATION_FAIL, CREATE_DONATION_REQUEST, CREATE_DONATION_SUCCESS } from '../constants/donationConstants';
+export const CreateDonation = (donation) => async (dispatch) => {
+    try {
+      dispatch({
+        type: CREATE_DONATION_REQUEST,
+      });
+  
+      const { data } = await axios.post(
+        `http://localhost:5000/api/create-donation`,donation
+      );
+  
+      dispatch({
+        type: CREATE_DONATION_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: CREATE_DONATION_FAIL,
+        error: error.response,
+      });
+    }
+  };
