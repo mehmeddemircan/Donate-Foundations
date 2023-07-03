c00762847f8b7d6e8a9ab96fba668a2d8f65d35d
mehmet demircan <mehmeddemircan@gmail.com>
2023-07-03 10:19:41 +0300

    removed category list card

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -32,7 +32,7 @@ const HomePage = () => {
       
         <HomeContentSection />
         <hr />
-        <CategoryList />
+        {/* <CategoryList /> */}
         <LogoGrid />
     </MainLayout>
   )
